3a127d00b90ea64cee92f870667b16ac2fdabfe1
Tadas Gricius <griciustadas@gmail.com>
2021-01-08 15:38:49 +0200

    Intro style changes

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -10,10 +10,16 @@ const Intro = () => {
   }
   return (
     <div className="box-layout">
-      <div className="box-layout__box">
+      <div className="box-layout__box box-layout__box--intro">
         <div className="box-layout__box__complete">
           <h1>Attention!</h1>
-          <p>Following system is created to analyze new graphical authentication system. for Collected data will be used for statistics and will be deleted from the database one week after testing is completed.</p>
+          <p>
+            Following system is created for university project to balance usability and security in password based authentication system.
+            This is an imitation of real life email authentication system.
+            <br /> <br />
+            Collected data will be used for statistics and will be deleted from the database one week after testing is completed.
+          </p>
+          <p style={{ color: 'red' }}>Please, do not use your real password while creating new account!</p>
           <button className="button" onClick={handleGetStarted}>Get Started</button>
         </div>
       </div>
