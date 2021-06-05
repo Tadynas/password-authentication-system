64a842f971686f4ace568d8cf13d0a925022d3c4
Tadas Gricius <griciustadas@gmail.com>
2021-06-05 14:42:51 +0300

    Configuring pexels search keys

--- src/api/pexels.js
+++ src/api/pexels.js
@@ -1,7 +1,7 @@
 import { createClient } from 'pexels'
 
 const client = createClient(process.env.PEXELS_CLIENT_KEY)
-const query = 'Nature, City, Animals, Art'
+const query = 'Nature, City, Animals'
 const per_page = 4
 
 const suffix = '?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500'
