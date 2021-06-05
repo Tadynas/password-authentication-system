ea1d8b0789b71d15d251e29e065c5649345b3aff
Tadas Gricius <griciustadas@gmail.com>
2021-06-05 14:56:10 +0300

    broaden picture query

--- src/api/pexels.js
+++ src/api/pexels.js
@@ -1,6 +1,6 @@
 import { createClient } from 'pexels'
 const client = createClient(process.env.REACT_APP_PEXELS_CLIENT_KEY)
-const query = 'Nature, City, Animals'
+const query = 'Nature, City, Animals, Art'
 const per_page = 4
 
 const suffix = '?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500'
